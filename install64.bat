970a5e3579b40f10bbe568cb1876bfdf0dd62445
Richard Barros <richardbarros@duck.com>
2022-10-21 17:08:01 -0300

    Instalador Oracle Data Access x64

---
+++ install64.bat
@@ -0,0 +1,9 @@
+echo @off
+
+cd .\x64
+call install.bat odp.net4 c:\oracle64 odp64
+setx /M path "%path%;C:\oracle64"
+C:\oracle64\odp.net\bin\4\OraProvCfg.exe /action:gac /providerpath:C:\oracle64\odp.net\bin\4\Oracle.DataAccess.dll
+C:\oracle64\odp.net\bin\4\OraProvCfg.exe /action:config /force /product:odp /frameworkversion:v4.0.30319 /providerpath:C:\oracle64\odp.net\bin\4\Oracle.DataAccess.dll
+
+echo Done
