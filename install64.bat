6d709d296a790cd40d621f277af4c407bee4c24a
Richard Barros <richardbarros@duck.com>
2022-10-27 11:18:34 -0300

    Deletar arquivos de instalação

--- install64.bat
+++ install64.bat
@@ -6,5 +6,6 @@ call install.bat odp.net4 c:\oracle64 odp64
 setx /M path "%path%;C:\oracle64"
 C:\oracle64\odp.net\bin\4\OraProvCfg.exe /action:gac /providerpath:C:\oracle64\odp.net\bin\4\Oracle.DataAccess.dll
 C:\oracle64\odp.net\bin\4\OraProvCfg.exe /action:config /force /product:odp /frameworkversion:v4.0.30319 /providerpath:C:\oracle64\odp.net\bin\4\Oracle.DataAccess.dll
-
+cd ..
+rmdir /s /q .\x64
 echo Done
